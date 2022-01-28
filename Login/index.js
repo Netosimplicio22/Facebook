668502c8109ed763630a570baf97bbfd3e517bc7
Netosimplicio22 <93277732+Netosimplicio22@users.noreply.github.com>
2022-01-28 02:25:21 -0300

    commit-facebook

---
+++ Login/index.js
@@ -0,0 +1,15 @@
+function border1(){
+    var input = document.getElementById("usuario")
+    var input1 = document.getElementById("password")
+
+    input.style.border = "1px solid #1877F2"
+    input1.style.border = "1px solid #dddfe2"
+}
+
+function border2(){
+    var input = document.getElementById("password")
+    var input1 = document.getElementById("usuario")
+
+    input.style.border = "1px solid #1877F2"
+    input1.style.border = "1px solid #dddfe2"
+}
